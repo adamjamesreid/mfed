24f46f62262623aa474b75dcfdf01fa56d151c1a
adamjamesreid <ajr236@cam.ac.uk>
2022-07-19 11:47:47 +0100

    Updated output files from R script

--- bin/mfed_diffbind.R
+++ bin/mfed_diffbind.R
@@ -31,8 +31,9 @@ gtf_file
 annotation_level
 
 # Comma-separated file listing priority of annotations, default used if not supplied
-#e.g. "Exon,Intron,5UTR,3UTR,Promoter,Downstream,Intergenic" 
-genomicAnnotationPriorityfile <- args[8] # Comma-separated file listing priority of annotations, default used if not supplied
+#e.g. "Exon,Intron,5UTR,3UTR,Promoter,Downstream,Intergenic"
+#genomicAnnotationPriorityfile <- args[8] # Comma-separated file listing priority of annotations, default used if not supplied
+genomicAnnotationPriorityfile  <- 'mfed/annotation_priority.csv'
 
 tssRegion = c(-100, 100)
 
@@ -47,8 +48,12 @@ info <- dba.show(db)
 libsizes <- cbind(LibReads=info$Reads, FRiP=info$FRiP,PeakReads=round(info$Reads * info$FRiP))
 rownames(libsizes) <- info$ID
 
-pdf("plot.pdf")
-plot(db)
+pdf("sample_heatmap_plot.pdf")
+dba.plotHeatmap(db)
+dev.off()
+
+pdf("sample_pca_plot.pdf")
+dba.plotPCA(db)
 dev.off()
 
 db <- dba.normalize(db)
@@ -77,9 +82,9 @@ dba.plotVolcano(db, th=as.double(fdr_cut))
 dev.off()
 
 #dev.off()
-pdf("sample_heatmap_post_contrast.pdf")
-plot(db, contrast=1)
-dev.off()
+#pdf("sample_heatmap_post_contrast.pdf")
+#plot(db, contrast=1)
+#dev.off()
 
 db.DB <- dba.report(db)
 db.DB
@@ -87,37 +92,25 @@ db.DB
 hist(db.DB$Fold, breaks=50)
 
 # Write out all results
-write.table(db.DB, file="results_all.txt", sep="\t", quote=FALSE, row.names=FALSE)
+# write.table(db.DB, file="results_all.txt", sep="\t", quote=FALSE, row.names=FALSE)
 
 # Filter for significance (fold change and FDR)
 db.DB.conf <- subset(db.DB, ((db.DB$Fold >= as.double(fc_cut) | db.DB$Fold <= as.double(fc_cut)) & db.DB$FDR <= as.double(fdr_cut)))
-write.table(db.DB.conf, file="results.txt", sep="\t", quote=FALSE, row.names=FALSE)
 
-# Write out significant peaks 
-db.DB.conf.df <- as.data.frame(db.DB.conf)  
-out_table <- db.DB.conf.df[, c("seqnames", "start", "end")]
-write.table(out_table, sep='\t', quote=FALSE, row.names=FALSE, col.names=FALSE, file="significant_fragments.bed")
+# Write out all significant results - enriched in fusion or control
+write.table(db.DB.conf, file="results_all.tsv", sep="\t", quote=FALSE, row.names=FALSE)
 
-# Write out consensus peak set
-dba.peakset(db, peak.format="bed", bRetrieve=TRUE, writeFile="filtered_fragments_diffbind.bed")
+# Write out peaks significantly enriched in fusion
+db.DB.conf.enrich.df <- as.data.frame(subset(db.DB.conf, db.DB.conf$Fold > 0))
+out_table <- db.DB.conf.enrich.df[, c("seqnames", "start", "end")]
+write.table(out_table, sep='\t', quote=FALSE, row.names=FALSE, col.names=FALSE, file="enriched_fragments.bed")
+
+# Write out results significantly enriched in fusion
+write.table(subset(db.DB.conf, db.DB.conf$Fold > 0), file="results_enirched_in_fusion.tsv", sep="\t", quote=FALSE, row.names=FALSE)
 
 # Testing to add gene annotation for fragments
 #library(TxDb.Dmelanogaster.UCSC.dm6.ensGene) # Which mitochondrial sequence does this annotation use?
 library(ChIPseeker)
-
-# N.b. some fragments do not get annotated, but in the case of the test data these were
-# all on non-primary sequence e.g.unplaced contigs, which presumably have little or no
-# annotation
-
-# Thinking that controling the reference going in and making custom annotations is the way to go
-#peakAnno.edb <- annotatePeak(db.DB.conf, tssRegion=c(-100, 100),
-#                             TxDb=TxDb.Dmelanogaster.UCSC.dm6.ensGene, #,annoDb="org.Hs.eg.db",
-#                  genomicAnnotationPriority = c("Exon", "Intron", "5UTR", "3UTR", "Promoter",  
-#                              "Downstream", "Intergenic"),
-#                  level="gene")
-
-#peakAnno.df <- as.data.frame(peakAnno.edb)
-#write.table(peakAnno.edb, file="results_annotated.tsv", sep="\t", quote=FALSE, row.names=FALSE)
 
 # Try making a custom TxDB object
 library(GenomicFeatures)
@@ -128,7 +121,7 @@ if(file.exists(genomicAnnotationPriorityfile)) {
   apf_df = read.csv(genomicAnnotationPriorityfile, header=FALSE)
 
   # Annotate fragments
-  peakAnno.custom.edb <- annotatePeak(db.DB.conf, tssRegion=tssRegion,
+  peakAnno.custom.edb <- annotatePeak(subset(db.DB.conf, db.DB.conf$Fold > 0), tssRegion=tssRegion,
                                       TxDb=Dm_custom_TXDb, #,annoDb="org.Hs.eg.db",
                                       genomicAnnotationPriority = as.character(apf_df[1,]),
                                       level=annotation_level)
@@ -145,5 +138,6 @@ peakAnno.custom.edb
 
 peakAnno.custom.df <- as.data.frame(peakAnno.custom.edb)
 write.table(peakAnno.custom.edb, file="results_annotated.tsv", sep="\t", quote=FALSE, row.names=FALSE)
+
 
 
